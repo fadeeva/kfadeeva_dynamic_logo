29004cf2483b238632b14493b469fdf941fbcb05
Katya Fadeeva <katfadmail@gmail.com>
2017-02-13 19:10:33 +0200

    circles

--- script/kfadeeva_logo.js
+++ script/kfadeeva_logo.js
@@ -3,9 +3,7 @@ window.onload = function() {
     if (theCanvas && theCanvas.getContext) {
         var ctx = theCanvas.getContext("2d");
         if (ctx) {
-             
-            var degrees = 173;
-            var radians = (Math.PI /180) * degrees;
+            
             var radius = 9;
             var x = 0;
             var y = 0;
@@ -18,24 +16,34 @@ window.onload = function() {
             }
             
             x = 200;
-            y = 120;
+            y = 130;
             drawCircle(x, y);
             
             x = 160;
-            y = 250;
+            y = 260;
             drawCircle(x, y);
             
             x = 238;
-            y = 250;
+            y = 260;
             drawCircle(x, y);
             
             x = 120;
-            y = 175;
+            y = 185;
             drawCircle(x, y);
             
             x = 280;
-            y = 175;
+            y = 185;
             drawCircle(x, y);
+            
+            // circle-border
+            radius = 100;
+            x = theCanvas.width / 2;
+            y = theCanvas.height / 2;
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "#3a3a3a";
+            ctx.beginPath();
+            ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+            ctx.stroke();
         }
     }
 }
