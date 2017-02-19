4292b579ddae6f5c73bfd669d864005e285a11fa
Katya Fadeeva <katfadmail@gmail.com>
2017-02-19 22:33:06 +0200

    buffered animation?

--- script/kfadeeva_logo.js
+++ script/kfadeeva_logo.js
@@ -1,52 +1,58 @@
 window.onload = function() { init(); }
 
-var theCanvas = null;
-var ctx = null;
+var canvas = null;
+var canvas_ctx = null;
+var bufferCanvas = null;
+var bufferCanvas_ctx = null;
 var startCoordinates = [ [200, 130], [280, 185], [238, 260], [160, 260], [120, 185]];
 
 function init() {
-    theCanvas = document.getElementById('canvas');
+    canvas = document.getElementById("canvas");
+    canvas_ctx = canvas.getContext("2d");
     
-    if (theCanvas && theCanvas.getContext) {
-        ctx = theCanvas.getContext("2d");
-        ctx.fillStyle = "#c3a157";
-        ctx.strokeStyle = "#c3a157";
-        ctx.lineWidth = 2;
-        
-        drawPentagon(startCoordinates);
-        
-        // outer circle border
-        radius = 100;
-        x = theCanvas.width / 2;
-        y = theCanvas.height / 2;
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = "#3a3a3a";
-        ctx.beginPath();
-        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-        ctx.stroke();
-    }
+    bufferCanvas = document.createElement("canvas");
+    bufferCanvas_ctx = bufferCanvas.getContext("2d");
+    bufferCanvas_ctx.canvas.width = canvas_ctx.canvas.width;
+    bufferCanvas_ctx.canvas.height = canvas_ctx.canvas.height;
     
+    canvas_ctx.fillStyle = "#c3a157";
+    canvas_ctx.strokeStyle = "#c3a157";
+    canvas_ctx.lineWidth = 2;
+
+    drawPentagon(startCoordinates);
+    
+    setInterval(animate, 30);
+    
+    // outer circle border
+    radius = 100;
+    x = canvas.width / 2;
+    y = canvas.height / 2;
+    canvas_ctx.lineWidth = 1;
+    canvas_ctx.strokeStyle = "#3a3a3a";
+    canvas_ctx.beginPath();
+    canvas_ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+    canvas_ctx.stroke();    
 }
 
 function drawCircle(x, y) {
     var radius = 9;
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-    ctx.fill();
+    canvas_ctx.beginPath();
+    canvas_ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+    canvas_ctx.fill();
     
 }
 
 function drawLines(coordinates) {
-    ctx.beginPath();
+    canvas_ctx.beginPath();
     coordinates.forEach(function(xy, i, coordinates) {
         if(i) {
-            ctx.lineTo(xy[0], xy[1]);
+            canvas_ctx.lineTo(xy[0], xy[1]);
         } else {
-            ctx.moveTo(xy[0], xy[1]);
+            canvas_ctx.moveTo(xy[0], xy[1]);
         }
     })
-    ctx.closePath();
-    ctx.stroke();
+    canvas_ctx.closePath();
+    canvas_ctx.stroke();
 }
 
 function drawPentagon(coordinates) {
@@ -56,5 +62,12 @@ function drawPentagon(coordinates) {
     drawLines(coordinates);
 }
 
+function blank() {
+    bufferCanvas_ctx.fillRect(0, 0, bufferCanvas_ctx.canvas.width, bufferCanvas_ctx.canvas.height);
+}
+
+function animate() {
+    blank();
+}
 
     
