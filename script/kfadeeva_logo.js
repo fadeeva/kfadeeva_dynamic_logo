54b8ce73a89055a081374f5f98278d222c3d773f
Katya Fadeeva <katfadmail@gmail.com>
2017-02-20 23:05:58 +0200

    animated one circle

--- script/kfadeeva_logo.js
+++ script/kfadeeva_logo.js
@@ -10,32 +10,27 @@ function init() {
     canvas = document.getElementById("canvas");
     canvas_ctx = canvas.getContext("2d");
     
+    /*
     bufferCanvas = document.createElement("canvas");
     bufferCanvas_ctx = bufferCanvas.getContext("2d");
     bufferCanvas_ctx.canvas.width = canvas_ctx.canvas.width;
     bufferCanvas_ctx.canvas.height = canvas_ctx.canvas.height;
+    */
     
+    /*
     canvas_ctx.fillStyle = "#c3a157";
     canvas_ctx.strokeStyle = "#c3a157";
     canvas_ctx.lineWidth = 2;
-
+    */
     drawPentagon(startCoordinates);
     
     setInterval(animate, 30);
-    
-    // outer circle border
-    radius = 100;
-    x = canvas.width / 2;
-    y = canvas.height / 2;
-    canvas_ctx.lineWidth = 1;
-    canvas_ctx.strokeStyle = "#3a3a3a";
-    canvas_ctx.beginPath();
-    canvas_ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-    canvas_ctx.stroke();    
+  
 }
 
 function drawCircle(x, y) {
     var radius = 9;
+    canvas_ctx.fillStyle = "#c3a157";
     canvas_ctx.beginPath();
     canvas_ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
     canvas_ctx.fill();
@@ -43,6 +38,10 @@ function drawCircle(x, y) {
 }
 
 function drawLines(coordinates) {
+    
+    canvas_ctx.strokeStyle = "#c3a157";
+    canvas_ctx.lineWidth = 2;
+    
     canvas_ctx.beginPath();
     coordinates.forEach(function(xy, i, coordinates) {
         if(i) {
@@ -63,11 +62,82 @@ function drawPentagon(coordinates) {
 }
 
 function blank() {
-    bufferCanvas_ctx.fillRect(0, 0, bufferCanvas_ctx.canvas.width, bufferCanvas_ctx.canvas.height);
+    canvas_ctx.fillStyle = "#222222";
+    canvas_ctx.fillRect(0, 0, canvas_ctx.canvas.width, canvas_ctx.canvas.height);
+    
+    // outer circle border
+    canvas_ctx.lineWidth = 1;
+    canvas_ctx.strokeStyle = "#3a3a3a";
+    canvas_ctx.beginPath();
+    canvas_ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, 2 * Math.PI, false);
+    canvas_ctx.stroke();
+    
+    canvas_ctx.strokeRect(100, 100, 200, 200);
 }
 
+var direction = getDirection();
+var i = 0;
 function animate() {
     blank();
+    
+    if(direction[0] && direction[1]) {       
+        startCoordinates[0][0]+=2, startCoordinates[0][1]+=2;       
+    } else if(direction[0] || direction[1]) {       
+        if(direction[0]) {         
+            startCoordinates[0][0]+=2, startCoordinates[0][1]-=2;
+        } else {
+            startCoordinates[0][0]-=2, startCoordinates[0][1]+=2;
+        }    
+    } else if(!direction[0] && !direction[1]) {       
+        startCoordinates[0][0]-=2, startCoordinates[0][1]-=2;
+    }
+    
+    if(startCoordinates[0][0] > 300) {
+        if(startCoordinates[0][1] < 100) {
+            direction = [false, true];
+        } else if(startCoordinates[0][1] > 300) {
+            direction = [false, false];
+        } else {
+            direction[0] = false;
+        }
+    } else if(startCoordinates[0][0] < 100) {
+        if(startCoordinates[0][1] < 100) {
+            direction = [true, true];
+        } else if(startCoordinates[0][1] > 300) {
+            direction = [true, false];
+        } else {
+            direction[0] = true;
+        }
+    } else {
+        if(startCoordinates[0][1] < 100) {
+            direction = [true, true];
+        } else if(startCoordinates[0][1] > 300) {
+            direction = [true, false];
+        }
+    }
+
+    i++;
+    if(i > 50) {
+        i = 0;
+        direction = getDirection();
+    }
+    
+    drawPentagon(startCoordinates);
+}
+
+function getDirection() {
+    var randomInt = Math.floor(Math.random() * 101);
+    if(randomInt > 0 && randomInt < 26) {
+        return [true, true];
+    } else if(randomInt > 25 && randomInt < 51) {
+        return [true, false];
+    } else if(randomInt > 50 && randomInt < 76) {
+        return [false, true];
+    } else if(randomInt > 75 && randomInt < 101) {
+        return [false, false];
+    } else {
+        return [false, false];
+    }
 }
 
     
